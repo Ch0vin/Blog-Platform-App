1618b05f64b836f5f861fbf9b6ec9f3cced1f185
chovin <152864419+Ch0vin@users.noreply.github.com>
2025-07-25 13:10:34 +0300

    project setup

---
+++ blog/src/test/java/com/chovin/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chovin.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
